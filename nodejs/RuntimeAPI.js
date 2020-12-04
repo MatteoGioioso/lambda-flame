f5df1eab99d76cc8ede55f927db75d8a11ceb261
MatteoGioioso <gioioso.matteo@gmail.com>
2020-12-04 20:35:08 +0700

    fix(): move all the runtime calls into the RuntimeAPI class

--- nodejs/RuntimeAPI.js
+++ nodejs/RuntimeAPI.js
@@ -1,9 +1,26 @@
 const http = require('http');
 
+const {
+    AWS_LAMBDA_FUNCTION_NAME,
+    AWS_LAMBDA_FUNCTION_VERSION,
+    AWS_LAMBDA_FUNCTION_MEMORY_SIZE,
+    AWS_LAMBDA_LOG_GROUP_NAME,
+    AWS_LAMBDA_LOG_STREAM_NAME,
+    AWS_LAMBDA_RUNTIME_API,
+} = process.env
+
+
 class RuntimeAPI {
-    request(options) {
-        options.host = HOST
-        options.port = PORT
+    constructor() {
+        this.RUNTIME_PATH = '/2018-06-01/runtime'
+        const [HOST, PORT] = AWS_LAMBDA_RUNTIME_API.split(':')
+        this.HOST = HOST;
+        this.PORT = PORT
+    }
+
+    _request(options) {
+        options.host = this.HOST
+        options.port = this.PORT
 
         return new Promise((resolve, reject) => {
             let req = http.request(options, res => {
@@ -21,11 +38,50 @@ class RuntimeAPI {
         })
     }
 
+    async nextInvocation() {
+        const res = await this._request({ path: `${this.RUNTIME_PATH}/invocation/next` })
+
+        if (res.statusCode !== 200) {
+            throw new Error(`Unexpected /invocation/next response: ${JSON.stringify(res)}`)
+        }
+
+        if (res.headers['lambda-runtime-trace-id']) {
+            process.env._X_AMZN_TRACE_ID = res.headers['lambda-runtime-trace-id']
+        } else {
+            delete process.env._X_AMZN_TRACE_ID
+        }
+
+        const deadlineMs = +res.headers['lambda-runtime-deadline-ms']
+
+        let context = {
+            awsRequestId: res.headers['lambda-runtime-aws-request-id'],
+            invokedFunctionArn: res.headers['lambda-runtime-invoked-function-arn'],
+            logGroupName: AWS_LAMBDA_LOG_GROUP_NAME,
+            logStreamName: AWS_LAMBDA_LOG_STREAM_NAME,
+            functionName: AWS_LAMBDA_FUNCTION_NAME,
+            functionVersion: AWS_LAMBDA_FUNCTION_VERSION,
+            memoryLimitInMB: AWS_LAMBDA_FUNCTION_MEMORY_SIZE,
+            getRemainingTimeInMillis: () => deadlineMs - Date.now(),
+            callbackWaitsForEmptyEventLoop: true,
+        }
+
+        if (res.headers['lambda-runtime-client-context']) {
+            context.clientContext = JSON.parse(res.headers['lambda-runtime-client-context'])
+        }
+
+        if (res.headers['lambda-runtime-cognito-identity']) {
+            context.identity = JSON.parse(res.headers['lambda-runtime-cognito-identity'])
+        }
+
+        const event = JSON.parse(res.body)
+
+        return { event, context }
+    }
 
     async invokeResponse(result, context) {
-        const res = await request({
+        const res = await this._request({
             method: 'POST',
-            path: `${RUNTIME_PATH}/invocation/${context.awsRequestId}/response`,
+            path: `${this.RUNTIME_PATH}/invocation/${context.awsRequestId}/response`,
             body: JSON.stringify(result === undefined ? null : result),
         })
         if (res.statusCode !== 202) {
@@ -34,8 +90,8 @@ class RuntimeAPI {
     }
 
     async postError(path, err) {
-        const lambdaErr = toLambdaErr(err)
-        const res = await request({
+        const lambdaErr = this._toLambdaErr(err)
+        const res = await this._request({
             method: 'POST',
             path,
             headers: {
@@ -48,6 +104,26 @@ class RuntimeAPI {
             throw new Error(`Unexpected ${path} response: ${JSON.stringify(res)}`)
         }
     }
+
+    invokeError(err, context) {
+        return this.postError(`${this.RUNTIME_PATH}/invocation/${context.awsRequestId}/error`, err)
+    }
+
+    initError(err) {
+        return this.postError(`${this.RUNTIME_PATH}/init/error`, err)
+    }
+
+    _toLambdaErr(err) {
+        const { name, message, stack } = err
+        return {
+            errorType: name || typeof err,
+            errorMessage: message || ('' + err),
+            stackTrace: (stack || '').split('\n').slice(1),
+        }
+    }
+
 }
 
-module.exports = RuntimeAPI;
+module.exports = {
+    RuntimeAPI
+};
--- nodejs/index.js
+++ nodejs/index.js
@@ -1,21 +1,10 @@
-const http = require('http');
-
-const RUNTIME_PATH = '/2018-06-01/runtime'
-
+const {RuntimeAPI} = require("./RuntimeAPI");
 const CALLBACK_USED = Symbol('CALLBACK_USED')
-
 const {
-    AWS_LAMBDA_FUNCTION_NAME,
-    AWS_LAMBDA_FUNCTION_VERSION,
-    AWS_LAMBDA_FUNCTION_MEMORY_SIZE,
-    AWS_LAMBDA_LOG_GROUP_NAME,
-    AWS_LAMBDA_LOG_STREAM_NAME,
     LAMBDA_TASK_ROOT,
     _HANDLER,
-    AWS_LAMBDA_RUNTIME_API,
 } = process.env
-
-const [HOST, PORT] = AWS_LAMBDA_RUNTIME_API.split(':')
+const runtimeAPI = new RuntimeAPI()
 
 start()
 
@@ -24,7 +13,7 @@ async function start() {
     try {
         handler = getHandler()
     } catch (e) {
-        await initError(e)
+        await runtimeAPI.initError(e)
         return process.exit(1)
     }
     tryProcessEvents(handler)
@@ -42,94 +31,19 @@ async function tryProcessEvents(handler) {
 
 async function processEvents(handler) {
     while (true) {
-        const { event, context } = await nextInvocation()
+        const { event, context } = await runtimeAPI.nextInvocation()
         let result
         try {
             result = await handler(event, context)
         } catch (e) {
-            await invokeError(e, context)
+            await runtimeAPI.invokeError(e, context)
             continue
         }
         // const callbackUsed = context[CALLBACK_USED]
 
-        await invokeResponse(result, context)
+        await runtimeAPI.invokeResponse(result, context)
         console.log("exiting...")
         return process.prependOnceListener('beforeExit', () => tryProcessEvents(handler))
-    }
-}
-
-function initError(err) {
-    return postError(`${RUNTIME_PATH}/init/error`, err)
-}
-
-async function nextInvocation() {
-    const res = await request({ path: `${RUNTIME_PATH}/invocation/next` })
-
-    if (res.statusCode !== 200) {
-        throw new Error(`Unexpected /invocation/next response: ${JSON.stringify(res)}`)
-    }
-
-    if (res.headers['lambda-runtime-trace-id']) {
-        process.env._X_AMZN_TRACE_ID = res.headers['lambda-runtime-trace-id']
-    } else {
-        delete process.env._X_AMZN_TRACE_ID
-    }
-
-    const deadlineMs = +res.headers['lambda-runtime-deadline-ms']
-
-    let context = {
-        awsRequestId: res.headers['lambda-runtime-aws-request-id'],
-        invokedFunctionArn: res.headers['lambda-runtime-invoked-function-arn'],
-        logGroupName: AWS_LAMBDA_LOG_GROUP_NAME,
-        logStreamName: AWS_LAMBDA_LOG_STREAM_NAME,
-        functionName: AWS_LAMBDA_FUNCTION_NAME,
-        functionVersion: AWS_LAMBDA_FUNCTION_VERSION,
-        memoryLimitInMB: AWS_LAMBDA_FUNCTION_MEMORY_SIZE,
-        getRemainingTimeInMillis: () => deadlineMs - Date.now(),
-        callbackWaitsForEmptyEventLoop: true,
-    }
-
-    if (res.headers['lambda-runtime-client-context']) {
-        context.clientContext = JSON.parse(res.headers['lambda-runtime-client-context'])
-    }
-
-    if (res.headers['lambda-runtime-cognito-identity']) {
-        context.identity = JSON.parse(res.headers['lambda-runtime-cognito-identity'])
-    }
-
-    const event = JSON.parse(res.body)
-
-    return { event, context }
-}
-
-async function invokeResponse(result, context) {
-    const res = await request({
-        method: 'POST',
-        path: `${RUNTIME_PATH}/invocation/${context.awsRequestId}/response`,
-        body: JSON.stringify(result === undefined ? null : result),
-    })
-    if (res.statusCode !== 202) {
-        throw new Error(`Unexpected /invocation/response response: ${JSON.stringify(res)}`)
-    }
-}
-
-function invokeError(err, context) {
-    return postError(`${RUNTIME_PATH}/invocation/${context.awsRequestId}/error`, err)
-}
-
-async function postError(path, err) {
-    const lambdaErr = toLambdaErr(err)
-    const res = await request({
-        method: 'POST',
-        path,
-        headers: {
-            'Content-Type': 'application/json',
-            'Lambda-Runtime-Function-Error-Type': lambdaErr.errorType,
-        },
-        body: JSON.stringify(lambdaErr),
-    })
-    if (res.statusCode !== 202) {
-        throw new Error(`Unexpected ${path} response: ${JSON.stringify(res)}`)
     }
 }
 
@@ -173,33 +87,4 @@ function getHandler() {
             result.then(resolve, reject)
         }
     })
-}
-
-function request(options) {
-    options.host = HOST
-    options.port = PORT
-
-    return new Promise((resolve, reject) => {
-        let req = http.request(options, res => {
-            let bufs = []
-            res.on('data', data => bufs.push(data))
-            res.on('end', () => resolve({
-                statusCode: res.statusCode,
-                headers: res.headers,
-                body: Buffer.concat(bufs).toString(),
-            }))
-            res.on('error', reject)
-        })
-        req.on('error', reject)
-        req.end(options.body)
-    })
-}
-
-function toLambdaErr(err) {
-    const { name, message, stack } = err
-    return {
-        errorType: name || typeof err,
-        errorMessage: message || ('' + err),
-        stackTrace: (stack || '').split('\n').slice(1),
-    }
 }
