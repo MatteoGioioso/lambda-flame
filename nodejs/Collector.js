490c31b93960730478e1b89d6880b3dacdb1592a
MatteoGioioso <gioioso.matteo@gmail.com>
2020-12-04 21:21:21 +0700

    fix(): refactor collector
    - implement a class and add tmp folder cleanup
    - add subfolder for isolate files

---
+++ nodejs/Collector.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const flamebearer = require('./flamebearer')
+const S3 = require('aws-sdk/clients/s3')
+const Path = require('path');
+
+const s3Client = new S3();
+
+class Collector {
+    constructor() {
+        this.OUTPUT_PATH = `/tmp/${process.env.LAMBDA_FLAME_OUTPUT}`
+    }
+
+    async cleanUpFiles(){
+        const path = this.OUTPUT_PATH
+        if (fs.existsSync(path)) {
+            fs.readdirSync(path).forEach((file, index) => {
+                const curPath = Path.join(path, file);
+                fs.unlinkSync(curPath);
+            });
+            fs.rmdirSync(path);
+        }
+    }
+
+    async sendToDest(){
+        const hash = Date.now().toString()
+        const data = fs.readFileSync(`${this.OUTPUT_PATH}/flamegraph.html`)
+
+        const params = {
+            Bucket: process.env.DEST_BUCKET,
+            Key: `flamegraph_${hash}.html`,
+            Body: data,
+        };
+
+        const res = await s3Client.upload(params).promise()
+        console.log(res)
+    }
+
+    async generateFlameGraph(){
+        await flamebearer.fuelTheFire(`${this.OUTPUT_PATH}/isolate-output.json`)
+    }
+}
+
+const collector = new Collector();
+
+collector.generateFlameGraph().then(async () => {
+    await collector.sendToDest()
+    await collector.cleanUpFiles()
+})
