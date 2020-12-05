1de4cd6e959759abf0b25859bfcb240e466500e3
Matteo Gioioso <gioioso.matteo@gmail.com>
2020-12-05 21:35:03 +0700

    Feature/layer (#7)
    * feat(): transform into a layer
    
    - save all files into a unique folder instead of just the html
    
    * chore(): move package* into layer
    
    * refactor(): move layer into sub-folder
    
    this way I can save other files outside
    
    * feat(): add template and script for deployment of SLSrepo
    
    * feat(): transform into a shared layer

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export APPNAME=lambda-flame
+PROJECT=${APPNAME}
+BUCKET=${PROJECT}-artifacts
+REGION=ap-southeast-1
+TEMPLATE_NAME=template
+PROFILE=default
+
+aws --profile "${PROFILE}" --region "${REGION}" s3 mb s3://"${BUCKET}"
+
+sam package --profile "${PROFILE}" --region "${REGION}"  \
+  --template-file ${TEMPLATE_NAME}.yaml \
+  --output-template-file output.yaml \
+  --s3-bucket "${BUCKET}"
+
+## the actual deployment step
+sam deploy --profile "${PROFILE}" --region "${REGION}" \
+  --template-file output.yaml \
+  --stack-name "${PROJECT}" \
+  --capabilities CAPABILITY_IAM \
+
+sam publish \
+    --template output.yaml \
+    --region "${REGION}" \
+    --semantic-version 1.0.0
